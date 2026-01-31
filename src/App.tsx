f06a259400c6dcb587b02d1bf5032e154087a284
Nicolas Torrent <ntorrent@expediagroup.com>
2026-01-31 15:24:59 +0100

    feat(03-01): integrate Button with green styling
    - Import and render Button below DateDisplay
    - Add .button class with green background (#22c55e)
    - Center button horizontally with auto margins
    - Add spacing with margin-top: 1rem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import DateDisplay from './components/DateDisplay'
+import Button from './components/Button'
 import './App.css'
 
 function App() {
   return (
     <>
       <DateDisplay />
+      <Button />
     </>
   )
 }
